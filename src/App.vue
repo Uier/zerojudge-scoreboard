58003ed5447bc4e6be6c3ec82098317c0e36148f
TzuWei <ap9940506@gmail.com>
2022-02-22 23:03:04 +0800

    mvp

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,110 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref, watchEffect } from 'vue';
+import { useStorage, useClipboard } from '@vueuse/core';
+
+const { copy, copied } = useClipboard();
+
+const uidStr = useStorage('uids', '58692, 48017, 52717');
+const pidStr = useStorage('pids', 'a001, a002, a003');
+
+const uids = ref([])
+const pids = ref([])
+
+watchEffect(() => {
+  uids.value = uidStr.value.split(', ')
+})
+watchEffect(() => {
+  pids.value = pidStr.value.split(', ')
+})
+
+const fetcher = computed(() => {
+  return `
+  const uids = [${uids.value.map(u => `"${u}"`).join(', ')}];\n
+  const pids = [${pids.value.map(p => `"${p}"`).join(', ')}];\n
+  const res = [];
+  const reqs = uids.map(
+    u => fetch('https://zerojudge.tw/UserStatistic?id=' + u)
+      .then(res => res.text())
+      .then(text => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(text, 'text/html');
+        const name = doc.querySelector('span > a').innerText;
+        const acs = doc.querySelectorAll('.acstyle');
+        const pid = []
+        for ( let i=0; i<acs.length; i++ ) {
+          pid.push(acs[i].innerText);
+        }
+        const pres = {}
+        for ( let i=0; i<pids.length; i++ ) {
+          if ( pid.includes(pids[i]) ) {
+            pres[pids[i]] = true;
+          } else {
+            pres[pids[i]] = false;
+          }
+        }
+
+        res.push({
+          uid: u,
+          name,
+          ...pres,
+        })
+      })
+  )
+  Promise.all(reqs).then(() => console.log('done')).then(() => console.log(res))
+  `.trim();
+});
+
+const res = ref('');
+const data = computed(() => {
+  return res.value ? JSON.parse(res.value) : '';
+});
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <div style="padding: 20px">
+    <div>
+      uids:
+      <textarea v-model="uidStr" style="width: 100%" />
+    </div>
+    <div>
+      pid:
+      <textarea v-model="pidStr" style="width: 100%" />
+    </div>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <span>login zerojudge and run this -> </span>
+    <button @click="copy(fetcher)">
+      <span v-if='!copied'>Copy</span>
+      <span v-else>Copied!</span>
+    </button>
+    <span> &lt;- in console</span>
+
+    <div style="padding-top: 10px">
+      copy object and paste it here:
+      <textarea v-model="res" style="width: 100%" />
+    </div>
+
+    <table v-if="data" style="padding-top: 20px;">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th v-for="p in pids" :key="p">{{ p }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="d in data" :key="d.no">
+          <td>{{ d.uid }}</td>
+          <td>{{ d.name }}</td>
+          <td
+            v-for="p in pids"
+            :key="p"
+            :style="{ backgroundColor: d[p] ? 'green' : 'transparent', textAlign: 'center' }"
+          >
+            {{ d[p] ? 'v' : 'x' }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
